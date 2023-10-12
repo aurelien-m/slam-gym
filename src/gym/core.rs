60685a24efbf2bc8240506664e76d934afec2966
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2023-10-13 00:16:36 +0200

    Rotation acceleration and deceleration

--- src/gym/core.rs
+++ src/gym/core.rs
@@ -19,14 +19,17 @@ pub struct Robot {
     pub position: Vec2,
     pub orientation: f32,
     pub velocity: f32,
-    pub trajectory: Vec<Vec2>,
+    pub angular_velocity: f32,
     pub sensor: Vec<SensorRay>,
     pub sensor_max_length: f32,
 }
 
+#[derive(Component)]
+pub struct Trajectory(pub Vec<Vec2>);
+
 const ACCELERATION: f32 = 50.0;
 const MAX_VELOCITY: f32 = 100.0;
-// const ANGULAR_ACCELERATION: f32 = PI / 2.0;
+const ANGULAR_ACCELERATION: f32 = PI / 2.0;
 const MAX_ANGULAR_VELOCITY: f32 = PI / 4.0;
 
 #[derive(Component)]
@@ -80,10 +83,12 @@ fn setup_robot(mut commands: Commands) {
         position: Vec2::new(0.0, 0.0),
         orientation: 0.0,
         velocity: 0.0,
-        trajectory: Vec::new(),
+        angular_velocity: 0.0,
         sensor_max_length: length,
         sensor,
     });
+
+    commands.spawn(Trajectory(Vec::new()));
 }
 
 fn update_physics(mut robots: Query<&mut Robot>, rapier_handlers: Query<&mut World>) {
@@ -121,70 +126,102 @@ fn update_physics(mut robots: Query<&mut Robot>, rapier_handlers: Query<&mut Wor
     }
 }
 
-fn update_robot(time: Res<Time>, mut robots: Query<&mut Robot>) {
+fn update_orientation(robot: &mut Robot, destination: &Vec2, dt: f32) -> bool {
+    let position = robot.position;
+    let delta_x = destination.x - position.x;
+    let delta_y = destination.y - position.y;
+    let target_angle = delta_y.atan2(delta_x);
+
+    let breaking_angle = (robot.angular_velocity.powi(2) / (2.0 * ANGULAR_ACCELERATION)).abs();
+    let angle_delta = (target_angle - robot.orientation).abs();
+
+    robot.angular_velocity = if angle_delta < breaking_angle {
+        robot.angular_velocity - ANGULAR_ACCELERATION * dt
+    } else if robot.angular_velocity < MAX_ANGULAR_VELOCITY {
+        robot.angular_velocity + ANGULAR_ACCELERATION * dt
+    } else {
+        MAX_ANGULAR_VELOCITY
+    };
+
+    let travelled_angle = robot.angular_velocity * dt;
+
+    if angle_delta > travelled_angle {
+        let new_orientation;
+
+        if (robot.orientation < target_angle && target_angle - robot.orientation <= PI)
+            || (robot.orientation > target_angle && robot.orientation - target_angle > PI)
+        {
+            new_orientation = robot.orientation + travelled_angle;
+        } else {
+            new_orientation = robot.orientation - travelled_angle;
+        }
+
+        let new_orientation = if new_orientation > PI {
+            new_orientation - 2.0 * PI
+        } else if new_orientation < -PI {
+            new_orientation + 2.0 * PI
+        } else {
+            new_orientation
+        };
+
+        robot.orientation = new_orientation;
+        return false;
+    } else {
+        robot.orientation = target_angle;
+    }
+
+    true
+}
+
+fn update_position(robot: &mut Robot, destination: &Vec2, dt: f32) -> bool {
+    let position = robot.position;
+    let delta_x = destination.x - position.x;
+    let delta_y = destination.y - position.y;
+    let target_angle = delta_y.atan2(delta_x);
+
+    let stopping_distance = (robot.velocity.powi(2) / (2.0 * ACCELERATION)).abs();
+    let distance = ((destination.x - robot.position.x).powi(2)
+        + (destination.y - robot.position.y).powi(2))
+    .sqrt();
+
+    robot.velocity = if stopping_distance > distance {
+        robot.velocity - ACCELERATION * dt
+    } else if robot.velocity < MAX_VELOCITY {
+        robot.velocity + ACCELERATION * dt
+    } else {
+        MAX_VELOCITY
+    };
+
+    let travelled_distance = robot.velocity * dt;
+
+    if distance > travelled_distance {
+        robot.position.x = robot.position.x + travelled_distance * target_angle.cos();
+        robot.position.y = robot.position.y + travelled_distance * target_angle.sin();
+    } else {
+        robot.position.x = destination.x;
+        robot.position.y = destination.y;
+        robot.velocity = 0.0;
+        return true;
+    }
+
+    return false;
+}
+
+fn update_robot(
+    time: Res<Time>,
+    mut robots: Query<&mut Robot>,
+    mut trajectory: Query<&mut Trajectory>,
+) {
     let mut robot = robots.single_mut();
-    let mut velocity;
-    let target_angle;
-    let new_x;
-    let new_y;
+    let mut trajectory = trajectory.single_mut();
 
-    if let Some(destination) = robot.trajectory.first() {
-        let delta_x = destination.x - robot.position.x;
-        let delta_y = destination.y - robot.position.y;
-        target_angle = delta_y.atan2(delta_x);
-        let delta = MAX_ANGULAR_VELOCITY * time.delta_seconds();
-
-        if (robot.orientation - target_angle).abs() > delta {
-            let new_orientation;
-
-            if (robot.orientation < target_angle && target_angle - robot.orientation <= PI)
-                || (robot.orientation > target_angle && robot.orientation - target_angle > PI)
-            {
-                new_orientation = robot.orientation + delta;
-            } else {
-                new_orientation = robot.orientation - delta;
-            }
-
-            let new_orientation = if new_orientation > PI {
-                new_orientation - 2.0 * PI
-            } else if new_orientation < -PI {
-                new_orientation + 2.0 * PI
-            } else {
-                new_orientation
-            };
-
-            robot.orientation = new_orientation;
+    if let Some(destination) = trajectory.0.first() {
+        if !update_orientation(&mut robot, destination, time.delta_seconds()) {
             return;
         }
 
-        let stopping_distance = (robot.velocity.powi(2) / (2.0 * ACCELERATION)).abs();
-        let distance = (delta_x.powi(2) + delta_y.powi(2)).sqrt();
-
-        velocity = if stopping_distance > distance {
-            robot.velocity - ACCELERATION * time.delta_seconds()
-        } else if robot.velocity < MAX_VELOCITY {
-            robot.velocity + ACCELERATION * time.delta_seconds()
-        } else {
-            MAX_VELOCITY
-        };
-
-        let travelled_distance = velocity * time.delta_seconds();
-
-        if distance > travelled_distance {
-            new_x = robot.position.x + travelled_distance * target_angle.cos();
-            new_y = robot.position.y + travelled_distance * target_angle.sin();
-        } else {
-            new_x = destination.x;
-            new_y = destination.y;
-            velocity = 0.0;
-            robot.trajectory.remove(0);
+        if update_position(&mut robot, destination, time.delta_seconds()) {
+            trajectory.0.remove(0);
         }
-    } else {
-        return;
     }
-
-    robot.orientation = target_angle;
-    robot.velocity = velocity;
-    robot.position.x = new_x;
-    robot.position.y = new_y;
 }
--- src/gym/ui.rs
+++ src/gym/ui.rs
@@ -21,7 +21,7 @@ impl Plugin for UiPlugin {
             .add_systems(
                 Update,
                 (
-                    update_camera,
+                    update_camera.run_if(input_pressed(MouseButton::Right)),
                     text_update_system,
                     add_trajectory_point.run_if(input_just_pressed(MouseButton::Right)),
                     change_show_fps.run_if(input_just_pressed(KeyCode::F)),
@@ -54,17 +54,14 @@ fn setup(mut commands: Commands) {
 
 fn update_camera(
     mut cameras: Query<&mut Transform, With<Camera2d>>,
-    mouse_input: Res<Input<MouseButton>>,
     mut cursor_moved_events: EventReader<CursorMoved>,
 ) {
     let mut camera = cameras.iter_mut().next().unwrap();
     let mut cursor_iter = cursor_moved_events.iter();
 
     if let (Some(first_cursor), Some(last_cursor)) = (cursor_iter.next(), cursor_iter.last()) {
-        if mouse_input.pressed(MouseButton::Left) {
-            camera.translation.x -= last_cursor.position.x - first_cursor.position.x;
-            camera.translation.y += last_cursor.position.y - first_cursor.position.y;
-        }
+        camera.translation.x -= last_cursor.position.x - first_cursor.position.x;
+        camera.translation.y += last_cursor.position.y - first_cursor.position.y;
     }
 }
 
@@ -74,7 +71,7 @@ fn add_trajectory_point(
     mut materials: ResMut<Assets<ColorMaterial>>,
     windows: Query<&Window, With<PrimaryWindow>>,
     cameras: Query<(&Camera, &GlobalTransform)>,
-    mut robots: Query<&mut core::Robot>,
+    mut trajectory: Query<&mut core::Trajectory>,
 ) {
     let (camera, camera_transform) = cameras.single();
     if let Some(world_position) = windows
@@ -83,25 +80,20 @@ fn add_trajectory_point(
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
         .map(|ray| ray.origin.truncate())
     {
-        let mut robot = robots.single_mut();
-        robot
-            .trajectory
+        let mut trajectory = trajectory.single_mut();
+        trajectory
+            .0
             .push(Vec2::new(world_position.x, world_position.y));
 
-        if robot.trajectory.len() == 1 {
-            graphics::draw_point(
-                &mut commands,
-                &mut meshes,
-                &mut materials,
-                robot.trajectory[0],
-            )
-        } else if robot.trajectory.len() > 1 {
+        if trajectory.0.len() == 1 {
+            graphics::draw_point(&mut commands, &mut meshes, &mut materials, trajectory.0[0])
+        } else if trajectory.0.len() > 1 {
             graphics::draw_line(
                 &mut commands,
                 &mut meshes,
                 &mut materials,
-                robot.trajectory[robot.trajectory.len() - 2],
-                robot.trajectory[robot.trajectory.len() - 1],
+                trajectory.0[trajectory.0.len() - 2],
+                trajectory.0[trajectory.0.len() - 1],
             )
         }
     }
